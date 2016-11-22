905d13c2eec82efebab5cfe580723481f61735fe
Ronald Wright <logiconcepts819@gmail.com>
2016-11-22 14:17:27 -0600

    Implement intelligent spacing for fraction presentation

--- moments.js
+++ moments.js
@@ -1,4 +1,4 @@
-var stylizeFraction = function(fraction)
+var stylizeFraction = function(fraction, idx)
 {
     var s = 1;
     var n = fraction.numerator;
@@ -14,30 +14,33 @@ var stylizeFraction = function(fraction)
         s = -s;
     }
     return '<div class="fracsign inline-div">'+(s<0?'&ndash;':'+')+
-           '</div><div class="fracnum inline-div">'+n+
+           '</div><div class="fracnum inline-div" id="num'+idx+'">'+n+
            '</div><div class="fracslash inline-div">/'+
-           '</div><div class="fracden inline-div">'+d+'</div>';
+           '</div><div class="fracden inline-div" id="den'+idx+'">'+d+'</div>';
 }
 
 var createBlankTable = function(j, threek, polmom)
 {
     $('#moment_table_area').html(
         '<table id="moment_table"><thead><tr>'+
-        '<td id="moment_var_cell">j</td>'+
-        '<td id="moment_var_cell">3k</td>'+
-        '<td id="moment_cell">Moment</td></thead><tbody><tr>'+
-        '<td id="moment_var_cell">'+j+'</td>'+
-        '<td id="moment_var_cell">'+threek+'</td>'+
-        '<td id="moment_cell">'+stylizeFraction(polmom)+'</td>'+
+        '<td class="moment_var_cell">j</td>'+
+        '<td class="moment_var_cell">3k</td>'+
+        '<td class="moment_cell">Moment</td></thead><tbody><tr>'+
+        '<td class="moment_var_cell">'+j+'</td>'+
+        '<td class="moment_var_cell">'+threek+'</td>'+
+        '<td class="moment_cell">'+stylizeFraction(polmom, 0)+'</td>'+
         '</tr></tbody></table>');
+    return {numWidth: $('#num0').width(), denWidth: $('#den0').width()};
 }
 
-var appendToTable = function(j, threek, polmom)
+var appendToTable = function(j, threek, polmom, idx)
 {
+    var idx = $('.fracnum').length;
     $('#moment_table tr:last').after(
-        '<tr><td id="moment_var_cell">'+j+'</td>'+
-        '<td id="moment_var_cell">'+threek+'</td>'+
-        '<td id="moment_cell">'+stylizeFraction(polmom)+'</td></tr>');
+        '<tr><td class="moment_var_cell">'+j+'</td>'+
+        '<td class="moment_var_cell">'+threek+'</td>'+
+        '<td class="moment_cell">'+stylizeFraction(polmom, idx)+'</td></tr>');
+    return {numWidth: $('#num'+idx).width(), denWidth: $('#den'+idx).width()};
 }
 
 var emptyTableArea = function()
@@ -45,15 +48,30 @@ var emptyTableArea = function()
     $('#moment_table_area').empty();
 }
 
-var addToTable = function(j, threek, polmom)
+var Tracker = function()
 {
+    this.maxNumWidth = 0;
+    this.maxDenWidth = 0;
+}
+
+Tracker.prototype.addToTable = function(j, threek, polmom, maxwidths)
+{
+    var maxWidths;
     if ($('#moment_table').length)
     {
-        appendToTable(j, threek, polmom);
+        widths = appendToTable(j, threek, polmom);
     }
     else
     {
-        createBlankTable(j, threek, polmom);
+        widths = createBlankTable(j, threek, polmom);
+    }
+    if (this.maxNumWidth < widths.numWidth)
+    {
+        this.maxNumWidth = widths.numWidth;
+    }
+    if (this.maxDenWidth < widths.denWidth)
+    {
+        this.maxDenWidth = widths.denWidth;
     }
 }
 
@@ -72,10 +90,17 @@ var generateMoments = function(p, updateFunc)
 var generateMomentTable = function()
 {
     var p = parseInt($('#maxJInput').val());
+    var t = new Tracker();
+
+    $('.fracnum').width('auto');
+    $('.fracden').width('auto');
 
     emptyTableArea();
     generateMoments(p, function(j, threek, pmom)
     {
-        addToTable(j, threek, pmom);
+        t.addToTable(j, threek, pmom);
     });
+
+    $('.fracnum').width(t.maxNumWidth+'px');
+    $('.fracden').width(t.maxDenWidth+'px');
 }
